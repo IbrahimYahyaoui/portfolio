8bd0d36c613afaf1afd8cc1d23b187e7d91a3127
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-07-09 22:31:34 +0100

    --
    About Done start Project section

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
+import Projects from "./components/Projects";
 
 function App() {
   return (
     <>
       <Navbar />
-
       <Header />
-
       <About />
+      <Projects />
     </>
   );
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { MinusIcon } from "@heroicons/react/20/solid";
+import myPic from "../assets/me.png";
 
 const About = () => {
-  return <div className="z-50  text-white h-96 bg-red-800">About</div>;
+  return (
+    <div className=" bg-white pt-5 flex  items-center flex-col">
+      <div className="flex flex-col w-10 items-center">
+        <h1 className="text-2xl text-center fontOutfit">About</h1>
+        <MinusIcon className="w-4  " />
+      </div>
+      <div className="p-4 flex items-center flex-col">
+        <p className=" lg:w-1/2 text-center mb-8">
+          I'm a web developer and computer science student who specializes in
+          using React to build modern and efficient web applications. With a
+          solid foundation in various programming concepts and techniques, React
+          has become my preferred framework. I'm continuously enhancing my
+          skills in the rapidly evolving React ecosystem to stay at the
+          forefront of web development.
+        </p>
+        <div className="">
+          <img src={myPic} alt="me" className=" w-28" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default About;
