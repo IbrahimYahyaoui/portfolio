ec69ef40eb20c96d3745aadd0d77f099c1e61db5
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-07-11 23:17:27 +0100

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ const Projects = ({ name }) => {
     <div className="text-white flex items-center p-4 flex-col ">
       <SectionTitle name="Projects" />
       {/* project 1 */}
-      <div className="lg:w-2/3  border-4 border-white rounded-lg group hover:scale-101 transition-transform ">
+      <div className="lg:w-2/3  border-4 mt-4 border-white rounded-lg group hover:scale-101 transition-transform ">
         <div className="  rounded-lg   h-90 mt-4 p-4 flex  overflow-hidden items-center lg:justify-center  flex-col-reverse lg:flex-row">
           <div className="flex  flex-col    lg:w-2/3 h-full   ">
             <p className="text-4xl fontOutfit group-hover:scale-105 transition-transform mt-4 lg:mt-0">
@@ -21,7 +21,7 @@ const Projects = ({ name }) => {
               them to make an outfits , then you can share it with your
               followers and get feedback from them.
             </p>
-            <button className="w-full lg:w-52  justify-center h-8 flex  items-center bg-purple-800 rounded-md mt-4 ">
+            <button className="w-full lg:w-52 fontPoppins justify-center h-8 flex  items-center bg-purple-800 rounded-md mt-4 ">
               Take look{" "}
               <ArrowRightIcon className="w-4 ml-2 group-hover:-rotate-45 transition-all " />
             </button>
@@ -41,14 +41,14 @@ const Projects = ({ name }) => {
       <div className="lg:w-2/3 my-4 border-4 border-white rounded-lg group hover:scale-101 transition-transform ">
         <div className="  rounded-lg   h-90 mt-4 p-4 flex  overflow-hidden items-center lg:justify-center  flex-col-reverse lg:flex-row">
           <div className="flex  flex-col    lg:w-2/3 h-full   ">
-            <p className="text-4xl fontOutfit group-hover:scale-105 transition-transform mt-4 lg:mt-0">
+            <p className="text-4xl fontOutfit  group-hover:scale-105 transition-transform mt-4 lg:mt-0">
               TouaregVoyages
             </p>
             <p className=" pt-4 fontPoppins">
               website for travel agency assisting them in enhancing their online
               visibility and boosting their revenue.
             </p>
-            <button className="w-full lg:w-52  justify-center h-8 flex  items-center bg-purple-800 rounded-md mt-4 ">
+            <button className="w-full lg:w-52 fontPoppins justify-center h-8 flex  items-center bg-purple-800 rounded-md mt-4 ">
               Take look{" "}
               <ArrowRightIcon className="w-4 ml-2 group-hover:-rotate-45 transition-all " />
             </button>
@@ -76,7 +76,7 @@ const Projects = ({ name }) => {
               allowing owner to control his store without need for external
               platform.
             </p>
-            <button className="w-full lg:w-52  justify-center h-8 flex  items-center bg-purple-800 rounded-md mt-4 ">
+            <button className="w-full lg:w-52 fontPoppins justify-center h-8 flex  items-center bg-purple-800 rounded-md mt-4 ">
               Take look{" "}
               <ArrowRightIcon className="w-4 ml-2 group-hover:-rotate-45 transition-all " />
             </button>
