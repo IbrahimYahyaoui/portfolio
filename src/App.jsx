4c20f92256c045698daa73fc01a54e51dbfa0e77
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-07-13 16:30:51 +0100

    Update App.jsx
    make onload animation work

--- src/App.jsx
+++ src/App.jsx
@@ -9,24 +9,38 @@ import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
+  const [loader, setLoader] = useState(true);
 
   useEffect(() => {
-    // Listen for the window.onload event.
-    window.onload = () => {
+    const handleLoad = () => {
       setIsLoaded(true);
+      // prevent scroll
+    };
+    document.body.style.overflow = "hidden";
+    window.addEventListener("load", handleLoad);
+
+    return () => {
+      window.removeEventListener("load", handleLoad);
     };
   }, []);
 
+  useEffect(() => {
+    if (isLoaded) {
+      document.body.style.overflow = "auto";
+
+      setLoader(false);
+    }
+  }, [isLoaded]);
+
   return (
     <>
-      {!isLoaded && (
+      {loader && (
         <div>
-          <div className="absolute z-50 flex h-screen w-screen items-center justify-center bg-black">
-            <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-purple-500"></div>
+          <div className="absolute z-50 flex h-full w-full items-center justify-center bg-black text-white">
+            <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-purple-500 text-white"></div>
           </div>
         </div>
-      )}
-
+      )}{" "}
       <Analytics />
       <Navbar />
       <Header />
