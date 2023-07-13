5aa2bcebdc9ea2adc01be4954ada3eba4432c4bd
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-07-13 16:08:55 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -5,19 +6,26 @@ import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import { Analytics } from "@vercel/analytics/react";
+
 function App() {
-  window.addEventListener("load", onPageLoad);
-  function onPageLoad() {
-    const loader = document.querySelector(".loader");
-    loader.className += " hidden";
-  }
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  useEffect(() => {
+    // Listen for the window.onload event.
+    window.onload = () => {
+      setIsLoaded(true);
+    };
+  }, []);
+
   return (
     <>
-      <div>
-        <div className="loader flex h-screen w-screen items-center justify-center bg-black">
-          <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-purple-500"></div>
+      {!isLoaded && (
+        <div>
+          <div className="absolute flex h-screen w-screen items-center justify-center bg-black">
+            <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-purple-500"></div>
+          </div>
         </div>
-      </div>
+      )}
 
       <Analytics />
       <Navbar />
