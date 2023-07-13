7da7e04498a41396f78e26392c08c3f88de73df6
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-07-13 16:01:29 +0100

    Update App.jsx
    add onload page

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,19 @@ import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 import { Analytics } from "@vercel/analytics/react";
 function App() {
+  window.addEventListener("load", onPageLoad);
+  function onPageLoad() {
+    const loader = document.querySelector(".loader");
+    loader.className += " hidden";
+  }
   return (
     <>
+      <div>
+        <div className="loader flex h-screen w-screen items-center justify-center bg-black">
+          <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-purple-500"></div>
+        </div>
+      </div>
+
       <Analytics />
       <Navbar />
       <Header />
