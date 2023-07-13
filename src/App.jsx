54a5e8cd74f47725c27f6ba3d456f023e219dc72
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-07-13 17:59:21 +0100

    Update App.jsx
    try to fix onload animation

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,15 @@ import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
-  const [loader, setLoader] = useState(true);
 
   useEffect(() => {
     const handleLoad = () => {
       setIsLoaded(true);
-      // prevent scroll
+      document.body.style.overflow = "auto"; // Enable scrolling
     };
-    document.body.style.overflow = "hidden";
+
+    document.body.style.overflow = "hidden"; // Disable scrolling
+
     window.addEventListener("load", handleLoad);
 
     return () => {
@@ -24,31 +25,27 @@ function App() {
     };
   }, []);
 
-  useEffect(() => {
-    if (isLoaded) {
-      document.body.style.overflow = "auto";
-      setLoader(false);
-    }
-  }, [isLoaded]);
-
   return (
     <>
-      {loader && (
-        <div>
-          <div className="absolute z-50 flex h-full w-full items-center justify-center bg-black text-white">
-            <div className="spinner fontPacifico h-14 w-14 animate-spin border-4 border-purple-800 ">
-              IB
-            </div>
-          </div>
+      {!isLoaded && (
+        <div className="absolute z-50 flex h-screen w-full items-center justify-center bg-black text-white">
+          <div className="spinner fontPacifico animate-spin text-4xl">IB</div>
         </div>
-      )}{" "}
+      )}
+      <div
+        className={`${
+          isLoaded ? "opacity-100 transition-opacity" : "opacity-0"
+        } duration-1000 ease-in`}
+      >
+        <Navbar />
+        <Header />
+        <About />
+        <Projects />
+        <Contact />
+        <Footer />
+      </div>
+
       <Analytics />
-      <Navbar />
-      <Header />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
     </>
   );
 }
