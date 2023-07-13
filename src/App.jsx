00ec8342fb086f589fee66bb9d3fc994c4221cc8
ibrahim_yahyaoui <ibrahimbnhs@gmail.com>
2023-07-13 13:08:40 +0100

    --

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
-
+import { Analytics } from "@vercel/analytics/react";
 function App() {
   return (
     <>
+      <Analytics />
       <Navbar />
       <Header />
       <About />
